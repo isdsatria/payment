bff262795fcd68016d978a0f7d7acf6f5767c4e5
isdsatria <isdsatria@gmail.com>
2023-04-13 15:50:46 +0700

    add kafka dependency

--- src/main/java/com/achsat/travel/payment/PaymentApplication.java
+++ src/main/java/com/achsat/travel/payment/PaymentApplication.java
@@ -1,13 +1,24 @@
 package com.achsat.travel.payment;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class PaymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+	@Bean
+	public NewTopic PaymentStatus(){
+		return TopicBuilder.name("orders")
+				.partitions(3)
+				.compact()
+				.build();
 	}
 
 }
